c7c34dd9257dc1964a76ab9d8b869a28dccc4113
jeremyphilemon <git@jrmyphlmn.com>
2024-09-04 22:55:30 +0530

    Add deploy buttons

--- app/(preview)/page.tsx
+++ app/(preview)/page.tsx
@@ -4,7 +4,7 @@ import { useRef } from "react";
 import { Message } from "@/components/message";
 import { useScrollToBottom } from "@/components/use-scroll-to-bottom";
 import { motion } from "framer-motion";
-import { MasonryIcon, VercelIcon } from "@/components/icons";
+import { GitIcon, MasonryIcon, VercelIcon } from "@/components/icons";
 import Link from "next/link";
 import { useChat } from "ai/react";
 
@@ -118,6 +118,31 @@ export default function Home() {
           />
         </form>
       </div>
+
+      <motion.div
+        className="flex flex-row gap-4 items-center justify-between fixed bottom-6 text-xs "
+        initial={{ y: 20, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ delay: 1 }}
+      >
+        <Link
+          target="_blank"
+          href="https://github.com/vercel-labs/ai-sdk-preview-roundtrips"
+          className="flex flex-row gap-2 items-center border px-2 py-1.5 rounded-md hover:bg-zinc-100 dark:border-zinc-700 dark:hover:bg-zinc-800"
+        >
+          <GitIcon />
+          View Source Code
+        </Link>
+
+        <Link
+          target="_blank"
+          href="https://vercel.com/templates/next.js/ai-sdk-roundtrips"
+          className="flex flex-row gap-2 items-center bg-zinc-900 px-2 py-1.5 rounded-md text-zinc-50 hover:bg-zinc-950 dark:bg-zinc-100 dark:text-zinc-900 dark:hover:bg-zinc-50"
+        >
+          <VercelIcon size={14} />
+          Deploy with Vercel
+        </Link>
+      </motion.div>
     </div>
   );
 }
