9a2f08fae7acb3f2444f1a1a6b1991efb50ba122
jeremyphilemon <git@jrmyphlmn.com>
2024-09-05 01:28:02 +0530

    Update metadataBase url

--- app/(preview)/layout.tsx
+++ app/(preview)/layout.tsx
@@ -3,7 +3,7 @@ import { Metadata } from "next";
 import { Toaster } from "sonner";
 
 export const metadata: Metadata = {
-  metadataBase: new URL("https://ai-sdk-preview-roundtrips.vercel.dev"),
+  metadataBase: new URL("https://ai-sdk-preview-roundtrips.vercel.app"),
   title: "Automatic Tool Roundtrips Preview",
   description:
     "Automatically handle multiple tool call roundtrips using Vercel AI SDK",
